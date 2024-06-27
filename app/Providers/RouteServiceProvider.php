1e12666cae8b0099f05689f8d81f43dbfbc3abe4
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-06-27 23:28:09 +0900

    forms/indexの見た目修正

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/forms';
     public const OWNER_HOME = '/owner/dashboard';
 
 
