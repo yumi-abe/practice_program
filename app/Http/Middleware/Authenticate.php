fac6815d66e32c6befe6374b3192db1698780d04
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-06-27 12:48:42 +0900

    Middleware/Authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,9 +3,14 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Route;
+
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,8 +19,15 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        //認証されていない場合はリダイレクトする
         if (! $request->expectsJson()) {
-            return route('login');
+            // return route('login');
+            if(Route::is('owner.*')){
+                return route($this->owner_route);
+            }
+            if(Route::is('user.*')){ //合っているか確認
+                return route($this->user_route);
+            }
         }
     }
 }
