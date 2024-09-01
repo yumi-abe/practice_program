9cd870c953dc52f3202374eef14a5d885fab1424
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-09-01 22:19:44 +0900

    プロフィール項目に電話番号を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,13 @@
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
+            <!-- Phone -->
+            <div class="mt-4">
+                <x-input-label for="phone" :value="__('電話番号')" />
+                <x-text-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required  />
+                <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+            </div>
+
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
