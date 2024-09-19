398802d6ac3431b5af6d9ece0fe849cd2180c846
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-09-19 22:39:35 +0900

    access部分作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,6 +36,24 @@
       <x-price-section />
     </section>
 
+    <!-- access -->
+    <section id="access" class="bg-pastel-pink py-10">
+      <div class="text-center">
+        <h2 class="text-2xl font-bold text-brown-500 ml- mb-7">ACCESS</h2>
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="{{ asset('img/accessimg1x.png') }}" alt="accessImage">
+        <div class="ml-2">
+          <p>〒155-0031 東京都世田谷区北沢２丁目１０</p>
+          <p>下北沢駅より徒歩2分</p>
+          <p><a href="tel:+8131234-5678">03-1234-5678</a></p>
+          <x-primary-button>
+            <a href="https://goo.gl/maps/EmkYveF2nM8KhRkZ7" target="_blank">google map</a>
+          </x-primary-button>
+        </div>
+        
+      </div>
+    </section>
 
   </x-home-layout>
 </html>
