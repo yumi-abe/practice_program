2aacd9388d56c6e4d3ce53be91eea864d4710072
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-09-20 22:16:03 +0900

    予約詳細部分のデータ処理修正

--- resources/views/booking/show.blade.php
+++ resources/views/booking/show.blade.php
@@ -33,7 +33,7 @@
                                         <label for="plan_category" class="leading-7 text-sm text-gray-600">プラン選択</label>
                                     </div>
                                     <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value="">
-                                        {{ $plansId }}
+                                        {{ $plan->plan_name }}
                                     </div>
                                     </div>
                                     <div class="relative">
@@ -41,7 +41,7 @@
                                         <label for="cast_category" class="leading-7 text-sm text-gray-600">キャスト選択</label>
                                     </div>
                                     <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" value="">
-                                        {{ $castsId }}
+                                        {{ $cast->cast_name }}
                                     </div>
                                     </div>
                                     <div class="relative">
