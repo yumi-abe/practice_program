4c972f3c8df03df7ccd17528fa4bcf07b77fe062
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-10-21 09:54:13 +0900

    ngrokの設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use App\Models\Blog;
 use App\Models\CastCategory;
 use App\Models\PlanCategory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,8 +28,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+        if (env('APP_ENV') === 'ngrok') {
+            $url->forceScheme('https');
+        }
+        // if (config('app.env') !== 'local') {
+        //     URL::forceScheme('https');
+        // }
+
+        // Model::shouldBeStrict(! $this->app->isProduction());
+
         view()->composer('*', function ($view) {
             // $view->with('blogs', Blog::all());
             // $view->with('castProfile', CastCategory::all());
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/flatpickr.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Stack -->
         @stack('custom-css')
